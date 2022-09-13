2975bf8857d63cbb40e1b5c80379eb9999601b05
Ajay Sharma <AJAYKRSHARMA132@GMAIL.COM>
2022-09-13 16:14:01 +0530

    changed db

--- IpuDevHub/settings.py
+++ IpuDevHub/settings.py
@@ -99,12 +99,12 @@ WSGI_APPLICATION = 'IpuDevHub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('POSTGRES_NAME'),
+        'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('POSTGRES_HOST'),
         'PORT': env('POSTGRES_PORT'),
-    }
+    }   
 }
 
 
